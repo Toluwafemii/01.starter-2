b32bfb0ff9d19c28842c357bcae53a7596c62c63
Your Name <davidokaseun@gmail.com>
2024-11-30 10:37:03 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Start writing JavaScript here!
+
+const button = document.querySelector('button')
+const body = document.body
+
+button.addEventListener('click', event => {
+    body.classList.toggle('offsite-is-open')
+})
